43b53b813e573c3157de8c425dc9e37686d3cf8c
ManuelJiron <majiron@uamv.edu.ni>
2024-05-03 08:41:23 -0600

    Ingreso

--- EJERCICIOSFOR/EJERCICIOFOR7/ejerciciofor7.cpp
+++ EJERCICIOSFOR/EJERCICIOFOR7/ejerciciofor7.cpp
@@ -2,44 +2,41 @@
 de igual longitud y luego calcule la suma de ambos vectores. El
 resultado debe mostrarse en un tercer vector.*/
 
-#include<iostream>
-#include<vector>
-
+#include <iostream>
 using namespace std;
+#define MAX 3
+int vector1[MAX];
+int vector2[MAX];
+int suma_vectores[MAX];
 
 int main(int argc, char const *argv[])
 {
-    int longitud;
-    cout<<"Ingrese la longitud de los vectores: ";
-    cin>>longitud;
-
-    vector<int> vector1(longitud);
-    vector<int> vector2(longitud);
-    vector<int> vectorSuma(longitud);
-    
-    cout << "Ingrese los elementos del primer vector: " << endl;
-    for (int i = 0; i < longitud ; i++)
+    int num1, num2;
+    for (int i = 0; i < MAX; i++)
     {
-        cout << "Elemento " << i + 1 << endl;
-        cin >> vector1[i];
-    } 
-    cout << "Ingrese los elementos del segundo vector: " << endl;
-    for (int i = 0; i < longitud ; i++)
+        cout << "\ningresar numero " << i + 1 << " a vector 1: ";
+        cin >> num1;
+        vector1[i] = num1;
+        cout << "ingresar numero " << i + 1 << " a vector 2: ";
+        cin >> num2;
+        vector2[i] = num2;
+    }
+    for (int i = 0; i < MAX; i++)
     {
-        cout << "Elemento " << i + 1 << endl;
-        cin >> vector2[i];
-    } 
-    
-    for (int i = 0; i < longitud ; i++)
-    {
-        vectorSuma[i] = vector1[i] + vector2[i];
+        suma_vectores[i] = vector1[i] + vector2[i];
     }
 
-    cout << "La suma de los dos vectores es igual: " << endl;
-    for (int i = 0; i < longitud ; i++)
+    cout << "\nla suma de los dos vectores es: {";
+    for (int i = 0; i < MAX; i++)
     {
-        cout << vectorSuma[i] << " ";
+        if (i == MAX - 1)
+        {
+            cout << suma_vectores[i] << "}" << endl;
+        }
+        else
+        {
+            cout << suma_vectores[i] << ", ";
+        }
     }
-
     return 0;
 }
